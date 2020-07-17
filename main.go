7b21d13a8c8e1e71f44284f42d9c419ae439dd09
mmaks17 <mmaks17@gmail.com>
2020-07-17 16:20:06 +0300

    Update main.go

--- main.go
+++ main.go
@@ -24,55 +24,33 @@ func parcetovar(s string){
 	pri_tov = strings.TrimSpace(doc.Find(".card__price .card__priceval .price").First().Text())
 
 	file, err := os.OpenFile("test.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0777)
-    defer file.Close()
- 
-    if err != nil {
-        os.Exit(1)
+          defer file.Close()
+        if err != nil {
+          os.Exit(1)
 	}
-	
 	csvWriter := csv.NewWriter(file)
 	csvWriter.Write([]string {id_tov,pri_tov, name_tov})
-	
-    csvWriter.Flush()
-
-	fmt.Println("-----------------------"+ id_tov +"----------------------------------------------")
-	fmt.Println(name_tov)
-	fmt.Println(id_tov)
-	fmt.Println(pri_tov)
-	fmt.Println("********************************************************************************")
-	
+        csvWriter.Flush()
 }
 func parcekateg(s string){
 	doc, err := goquery.NewDocument(s)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	doc.Find(".prod__link").Each(func(index int, item *goquery.Selection) {
-	
 		tov_url,_ :=item.Attr("href")
-
-		//fmt.Println("url: " + tov_url)
 		parcetovar(MainURL+tov_url)
-	
     })
 }
-
 func parcemenu(s string){
 	doc, err := goquery.NewDocument(s)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	doc.Find(".fgrid__item-i").Each(func(index int, item *goquery.Selection) {
-	
 		kat_url,_ :=item.Attr("href")
-
-		//fmt.Println("url: " + kat_url)
 		parcekateg(MainURL+kat_url + "?a=1")
-	
     })
-
 }
 
 func main() {
