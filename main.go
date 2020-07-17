fa3642f5b8f8e91ed27c3a75c24290055879f43c
mokulov <mokulov@oggettoweb.com>
2020-07-17 13:43:12 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"encoding/csv"
+	"os"
+	"github.com/PuerkitoBio/goquery"
+)
+
+const MainURL = "https://somesite"
+func parcetovar(s string){
+	doc, err := goquery.NewDocument(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var id_tov string
+	var name_tov string
+	var pri_tov string 
+
+	id_tov = strings.Replace( doc.Find(".product-id").First().Text(), "ID товара: ", "",1)
+	name_tov = doc.Find("h1").Contents().Text()
+	pri_tov = strings.TrimSpace(doc.Find(".card__price .card__priceval .price").First().Text())
+
+	file, err := os.OpenFile("test.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0777)
+    defer file.Close()
+ 
+    if err != nil {
+        os.Exit(1)
+	}
+	
+	csvWriter := csv.NewWriter(file)
+	csvWriter.Write([]string {id_tov,pri_tov, name_tov})
+	
+    csvWriter.Flush()
+
+	fmt.Println("-----------------------"+ id_tov +"----------------------------------------------")
+	fmt.Println(name_tov)
+	fmt.Println(id_tov)
+	fmt.Println(pri_tov)
+	fmt.Println("********************************************************************************")
+	
+}
+func parcekateg(s string){
+	doc, err := goquery.NewDocument(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find(".prod__link").Each(func(index int, item *goquery.Selection) {
+	
+		tov_url,_ :=item.Attr("href")
+
+		//fmt.Println("url: " + tov_url)
+		parcetovar(MainURL+tov_url)
+	
+    })
+}
+
+func parcemenu(s string){
+	doc, err := goquery.NewDocument(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find(".fgrid__item-i").Each(func(index int, item *goquery.Selection) {
+	
+		kat_url,_ :=item.Attr("href")
+
+		//fmt.Println("url: " + kat_url)
+		parcekateg(MainURL+kat_url + "?a=1")
+	
+    })
+
+}
+
+func main() {
+
+    parcemenu(MainURL+"/catalog/")
+
+}
